faa9c971972b4ef6febb7a064c73cfb1c5fe4801
John Hewitt <jhnh198@gmail.com>
2024-04-01 21:07:24 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { Form, ListGroup, Stack } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <Stack direction="horizontal">
+      {/* add new concepts here */}
+      <ListGroup className='vr'>
+        <ListGroup.Item className=''>Nodes</ListGroup.Item>
+        <ListGroup.Item className=''>Nodes</ListGroup.Item>
+      </ListGroup>
+
+      {/* output of concept selection */}
+      <Form>
+        <Form.Control type="textarea" placeholder="Concept goes here..." readOnly />
+      </Form>
+    </Stack>      
+  )
+}
+
+export default App
