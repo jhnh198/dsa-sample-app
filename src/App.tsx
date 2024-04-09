3e1359d3fc9fb5c41bf482a48cbc425122b87f85
John Hewitt <jhnh198@gmail.com>
2024-04-08 21:23:57 -0500

    added buttons to display different components

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,35 @@ import { useRef, useState } from 'react'
 import './App.css'
 import { Form, ListGroup, Stack } from 'react-bootstrap'
 import { SampleLayout } from './components/SampleLayout';
+import { DS_Node } from './components/data-structures/Node/DS_Node';
+import DS_LinkedList from './components/data-structures/Linked List/DS_LinkedList';
 
 function App() {
-  const [data, setData] = useState([]);
-  const textRef = useRef();
+  const arrayOfSamples = [
+    <DS_Node />,
+    <DS_LinkedList />
+  ];
+
+  const [index, setIndex] = useState(0);
+  function handleOnClick(i: number){
+    setIndex(() => i)
+  }
 
   return (
     <Stack direction="horizontal">
       {/* add new concepts here */}
-      <ListGroup className='vr'>
-        <ListGroup.Item className=''>Nodes</ListGroup.Item>
-        <ListGroup.Item className=''>Linked List</ListGroup.Item>
-      </ListGroup>
+      <button onClick={() => handleOnClick(0)}>
+        Node
+      </button>
+      <button onClick={() => handleOnClick(1)}>
+        Linked List
+      </button>
+      <div>
+        {arrayOfSamples[index]}
+      </div>
 
       {/* output of concept selection */}
-      <SampleLayout />
+      
     </Stack>      
   )
 }
---
+++ src/components/data-structures/Linked List/DS_LinkedList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function DS_LinkedList(){
+  return (
+    <div>DS_LinkedList</div>
+  )
+}
+
+export default DS_LinkedList
