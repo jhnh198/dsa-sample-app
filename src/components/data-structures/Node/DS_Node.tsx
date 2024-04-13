d414aad0dc5997ffbf509002b62c0b1a0ac5b5a9
John Hewitt <jhnh198@gmail.com>
2024-04-12 21:52:34 -0500

    added null checking to node and value for data link

--- src/components/data-structures/Node/DS_Node.tsx
+++ src/components/data-structures/Node/DS_Node.tsx
@@ -1,39 +1,38 @@
-import { useEffect, useRef, useState } from "react"
+import {useRef, useState } from "react"
 
-/* type sampleNode = {
-  nodeData: string | undefined
-  nodeDataLink: sampleNode | undefined
-  nodes: <sampleNode>[]
-}
-
-type inputData = {
-  data: string | undefined
-}
-*/
-
-//todo: add node data link and input for it
 export function DS_Node(){
   const [nodeData, setNodeData] = useState('');
-  const [nodeDataLink, setNodeDataLink] = useState(null);
-  const [nodes, setNodes] = useState([]);
+  const [nodeDataLink, setNodeDataLink] = useState('');
 
   const dataRef = useRef<HTMLInputElement>(null);
 
-  //todo: clear these warnings
   function handleOnClick(){
-    setNodeData(() => dataRef.current?.value)
-    dataRef.current?.value = null;
+    if(dataRef.current){
+      setNodeData(dataRef.current.value)
+      dataRef.current.value = '';
+    } 
+  }
+
+  function addDataLink(){
+    if(dataRef.current){
+      setNodeDataLink(dataRef.current.value)
+      dataRef.current.value = '';
+    } 
   }
   
   return (
   <div>
     <p>A node is a basic unit of data that contains a value and a link to a new node.
       It is the building block of a Linked List and other data structures.
+      This is a single node. A linked node is in the linked list sample.
     </p>
     <input type="text" ref={dataRef}/>
     <button onClick={handleOnClick}> Set Node Data </button>
+
+    {/*todo: set up button to add a data link with the input */}
+    <button onClick={addDataLink}> Add Data Link </button>
       Data: {nodeData} <br/>
       Link: {nodeDataLink}
-    </div>
+  </div>
   )
 }
